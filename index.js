6325b8a21f0c2fcdcf927c711b3dc6de70548f2c
khaled8787 <khaledmahmud01737@gmail.com>
2025-08-27 18:17:52 +0600

    love clicked completed

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // nav section
-document.getElementsByClassName('fa-heart').addEventListener('click', function(){
-    console.log('clicked')
-})
+const hearts = document.getElementsByClassName('fa-heart');
+for(const heart of hearts){
+    heart.addEventListener('click', function(){
+       const loveCounter = document.getElementById('get-love')
+        let currentNumber = parseInt(loveCounter.innerText);
+        loveCounter.innerText = currentNumber + 1;
+    })
+}
